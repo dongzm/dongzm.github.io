98364fdfeba075be9ee03c6223253a35c00a5820
dongzm <15375855383@163.com>
2017-02-09 16:50:54 +0800

    修改命令

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,3 +1,19 @@
+@echo off
+title  推送修改到服务器
+echo.
+echo Select a task:
+echo =============
+echo.
+echo 1) 开始推送
+echo 2) 退出
+echo.
+set /p val=select value:
+if "%val%"=="1" goto push
+if "%val%"=="2" exit
+:push
+::进入git目录
+cd G:\sublime\dongzm.github.io
+g:
 ::上传master分支到远程仓库
 git push origin master:master
 ::chrome浏览器打开网站
