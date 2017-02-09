1027a6227b1cf564f7c1d8ea0fc3300e80207214
dongzm <15375855383@163.com>
2017-02-09 11:21:05 +0800

    [message]

---
+++ build.bat
@@ -0,0 +1,5 @@
+::进入warder项目慕目录
+cd G:\sublime\workspace\warder
+::构建项目编译打包成部署文件
+npm run build
+pause
--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,21 @@
+::删除上次的备份文件
+del G:\sublime\dongzm.github.io\backup\index.html
+rmdir /s/q G:\sublime\dongzm.github.io\backup\static
+::备份当前文件
+copy /y G:\sublime\dongzm.github.io\index.html g:\sublime\dongzm.github.io\backup
+md g:\sublime\dongzm.github.io\backup\static
+xcopy G:\sublime\dongzm.github.io\static g:\sublime\dongzm.github.io\backup\static/s
+::删除index.html文件和static文件夹
+del G:\sublime\dongzm.github.io\index.html
+rmdir /s/q G:\sublime\dongzm.github.io\static
 ::拷贝部署文件到直接文件夹
-copy /y .\dist\index.html g:\sublime\dongzm.github.io\
+copy /y G:\sublime\workspace\warder\dist\index.html g:\sublime\dongzm.github.io\
 ::创建static文件夹
 md g:\sublime\dongzm.github.io\static
 ::拷贝部署文件到直接文件夹
-xcopy .\dist\static g:\sublime\dongzm.github.io\static/s
+xcopy G:\sublime\workspace\warder\dist\static g:\sublime\dongzm.github.io\static/s
 ::回到当前git目录
-cd ../../dongzm.github.io
+cd G:\sublime\dongzm.github.io
 ::添加文件到git暂存区
 git add ./
 ::提交暂存区到仓库区
